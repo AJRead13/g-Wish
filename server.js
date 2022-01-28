5de1df5bac3c3946a2db328e37ceb30d7ba410fe
Andrew Read <aj.read13@gmail.com>
2022-01-28 13:22:46 -0500

    updated models index associations

--- server.js
+++ server.js
@@ -5,26 +5,26 @@ const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connections');
 const helpers = require('./utils/helpers')
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
-// const sess = {
-//   secret: process.env.SESSION_SECRET,
-//   cookie: {
-//     maxAge: 900000,
-//   },
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 900000,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
