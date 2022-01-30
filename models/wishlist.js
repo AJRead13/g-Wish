71fbe1ed8b2b8cf74419a888628e55a2c64b7b15
Warren Haskins <warrenhaskins1@gmail.com>
2022-01-30 15:22:01 -0500

    added fetch-api.js

--- models/wishlist.js
+++ models/wishlist.js
@@ -1,8 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connections');
+const sequelize = require('../config/connection');
 
-class WishList extends Model {}
+class WishList extends Model { };
 
 WishList.init(
     {
@@ -27,8 +27,6 @@ WishList.init(
                 key: "id"
             }
         },
-    },
-    {
         sequelize,
         timestamps: false,
         freezeTableName: true,
