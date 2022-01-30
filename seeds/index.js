f596fa4e9f30707aebcb114fc2bd441c08c8927c
Jose Dominguez <jdomindev@gmail.com>
2022-01-30 18:38:06 -0500

    pulling down new seeds data

--- seeds/index.js
+++ seeds/index.js
@@ -9,14 +9,14 @@ const seedAll = async () => {
   await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----\n');
   
-  await seedUsers();
-    console.log('\n----- USERS SEEDED -----\n');
+  // await seedUsers();
+  //   console.log('\n----- USERS SEEDED -----\n');
   
-  await seedPosts();
-    console.log('\n----- POSTS SEEDED -----\n');
+  // await seedPosts();
+  //   console.log('\n----- POSTS SEEDED -----\n');
 
-  await seedComments();
-    console.log('\n----- COMMENTS SEEDED -----\n');
+  // await seedComments();
+  //   console.log('\n----- COMMENTS SEEDED -----\n');
   
   await seedGames(); 
     console.log('\n----- GAMES SEEDED -----\n');   
